9f194ae1b2f3a2bded45454c1350646ccd91b1a8
kelangiraviteja <kelangiraviteja@gmail.com>
2022-02-06 18:54:34 +0530

    maven files

---
+++ amazon/Fridge/fridge/src/main/java/amazon/App.java
@@ -0,0 +1,14 @@
+package amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
